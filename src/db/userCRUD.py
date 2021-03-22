074d1b7f6a200ebdda156fe772e2cfd44e4b32e8
Brandon Goodhue <btg8847@g.rit.edu>
2021-03-22 17:40:08 -0400

    Finished basic CRUD for users

--- src/db/userCRUD.py
+++ src/db/userCRUD.py
@@ -23,3 +23,26 @@ def getUserByUsername(username):
     """
     return exec_get_one('SELECT * FROM users WHERE username = %s', [username])
 
+def deleteUserByUsername(username):
+    """
+    Deletes a user profile by their username
+    :param username: The username identifying the user to delete
+    :return:
+    """
+    exec_commit('DELETE FROM users WHERE username = %s', [username])
+
+def updateUser(username, password, last_access, new_username = ''):
+    """
+    Updates a user entry
+    :param username: The username to identify the use entry with
+    :param password: The user's new password
+    :param last_access: The last time the user account has been accessed
+    :param new_username: The user's new username (optional)
+    :return:
+    """
+    if not new_username: # If new_username hasn't been specified
+        new_username = username
+
+    hash = hashlib.sha256(password.encode('utf-8')).hexdigest()  # The encoded password
+    update_sql = "UPDATE users SET username = %s, password = %s, last_access_date = %s WHERE username = %s"
+    exec_commit(update_sql, [new_username, hash, last_access, username])
