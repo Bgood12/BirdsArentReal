c7262bad74085fd3d3b8ba1192eca7406490df5e
Lourdu-Bakon <ejl4635@rit.edu>
2021-03-28 22:14:35 -0400

    Split editIngredient into smaller functions for editing name and aisle, as discussed in the 03/28/2021 meeting

--- src/db/ingredientsCRUD.py
+++ src/db/ingredientsCRUD.py
@@ -1,7 +1,7 @@
 from src.db.db_utils import *
 
 
-def insertIngedient(name, aisle):
+def insertIngredient(name, aisle):
     insert_sql = 'INSERT INTO ingredients (ingredient_name, aisle) VALUES (%s, %s)'
     exec_commit(insert_sql, [name, aisle])
 
@@ -10,7 +10,17 @@ def deleteIngredient(id):
     exec_commit('DELETE FROM ingredients WHERE ingredient_id = %d', [id])
 
 
-def editIngrdient(id, name, aisle):
+def editName(id, name):
+    update_sql = 'UPDATE ingredients SET ingredient_name = %s WHERE ingredient_id = %d'
+    exec_commit(update_sql, [name, id])
+
+
+def editAisle(id, aisle):
+    update_sql = 'UPDATE ingredients SET aisle = %s WHERE ingredient_id = %d'
+    exec_commit(update_sql, [aisle, id])
+
+
+def editIngredient(id, name, aisle):
     update_sql = 'UPDATE ingredients SET ingredient_name = %s, aisle = %s WHERE ingredient_id = %d'
     exec_commit(update_sql, [name, aisle, id])
 
