21c5bd77eca42ebb15daa7013b590f0a84233747
Lourdu-Bakon <ejl4635@rit.edu>
2021-04-01 12:01:18 -0400

    additions to basic cli outline

--- src/program_operations_EL/commandParser.py
+++ src/program_operations_EL/commandParser.py
@@ -1,19 +1,22 @@
 from src.program_operations.accountOperations import *
 from src.program_operations.recipeOperations import *
 
+currentUser = None  # global variable for storing the current user
 
-currentUser = None # global variable for storing the current user
 
 def help(loggedin):
     # displays list of currently available commands
     if loggedin:
-        print("Available commands:\nq - quits the application\nhelp - display available commands\nlogout - logs the current user out")
+        print(
+            "Available commands:\nq - quits the application\nhelp - display available commands\nlogout - logs the current user out")
     else:
         helpmessage = "Available commands:\nq - quits the application\nhelp - display available commands\nlogin [username] [password] - logs the " \
                       "users into their accounts\nregister [username] [password] - registers a new user account\n"
         print(helpmessage)
-def parseInput(input):
-    command = input.split()
+
+
+def parseInput(inputStr):
+    command = inputStr.split()
     global currentUser
     if command[0] == "help":
         help(currentUser is not None)
@@ -26,6 +29,19 @@ def parseInput(input):
     else:
         if command[0] == "logout":
             currentUser.logout()
-        #elif:
+        elif command[0] == "createRecipe":
             # beginning of recipe operations
-
+            recipeName = inputStr[12:]
+            description = input("Enter a description: ")
+            cook_time = float(input("Prep time in minutes: "))
+            steps = input("What steps does it take to prepare? ")
+            difficulty = input("How hard is it to prepare [very_easy to very_hard]? ")
+            newRecipe(currentUser, recipeName, description, cook_time, steps, difficulty)
+        elif command[0] == "editRecipe":
+            recipeID = int(command[1])
+            # TODO add prompt for which part of the recipe is being edited, then a prompt for the new value
+        # TODO recipe operations: getMyRecipes (to view ids of recipes by the user), deleteRecipe [id]
+        # TODO createCategoryForRecipe [recipeID] [new-category], assignToCategory [recipeID] [category]
+        # TODO recipe search operations
+        # TODO cooking the recipe related operations
+        # TODO user pantry operations
