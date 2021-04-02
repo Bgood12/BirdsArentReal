a35f0ef7b153f75a4ce62d272f943493530df830
Lourdu-Bakon <ejl4635@rit.edu>
2021-04-02 19:22:44 -0400

    undo of previous fixes

--- src/program_operations_EL/cliMain.py
+++ src/program_operations_EL/cliMain.py
@@ -1,9 +1,8 @@
 import sys
-sys.path.clear()
-sys.path.insert(0, '../src')
 from src.program_operations_EL.commandParser import *
 
 
+print(sys.path)
 print("type 'help' to view commands")
 for line in sys.stdin:
     if 'quit' == line.rstrip():
